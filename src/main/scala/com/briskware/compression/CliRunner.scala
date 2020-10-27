3e0dd8e930a1f731ab41516bd06a60a4515e8957
pityupista <stefan@szaniszlo.info>
2020-10-27 02:26:51 +0000

    prototype of tree serialisation and de-serialisation working

--- src/main/scala/com/briskware/compression/CliRunner.scala
+++ src/main/scala/com/briskware/compression/CliRunner.scala
@@ -1,12 +1,15 @@
 package com.briskware.compression
 
 
+import java.io.{StringReader, StringWriter}
 import java.nio.charset.StandardCharsets
+
+import scala.util.{Success, Try}
 
 
 object CliRunner extends App  {
 
-  implicit val logger: Logger = Logger.getConsoleLogger(Logger.DEBUG)
+  implicit val logger: Logger = Logger.getConsoleLogger(Logger.TRACE)
 
   val compressor = ShannonFano()
 
@@ -26,6 +29,31 @@ object CliRunner extends App  {
 
   assert(decompressedString == textToCompress, "strings don't match")
 
+  val sWriter = new StringWriter()
+
+  logger.debug(s"Original tree: ${cData.tree}")
+
+  val io = new CompressorFileIO(cData)
+  io.writeTree(sWriter)
+
+  val serialisedTree = sWriter.getBuffer.toString
+
+  logger.info(s"Serialised tree: ${serialisedTree}")
+
+  //0 0 165 166 0 168 0 167 169
+  //0 0 141      142      0 144      0 143      145
+  //0 0 11000001 11000010 0 11000100 0 11000011 11000101
+
+  val sReader = new StringReader(serialisedTree)
+
+  val Success(tree) = CompressorFileIO.readTree(sReader)
+
+  logger.debug(s"Reconstructed tree: ${tree}")
+
+  val fWriter = new StringWriter()
+  io.writeAll(fWriter)
+
+  logger.info(s"All (lenght=${fWriter.getBuffer.toString.length}): ${fWriter.getBuffer.toString}")
 }
 
 // EOF
--- src/main/scala/com/briskware/compression/CompressorFileIO.scala
+++ src/main/scala/com/briskware/compression/CompressorFileIO.scala
@@ -1,21 +1,50 @@
 package com.briskware.compression
 
-import java.io.{File, FileOutputStream}
+import java.io._
 
+import scala.annotation.tailrec
 import scala.util.Try
 
 object CompressorFileIO {
+
+  def readTree(reader: Reader)(implicit logger: Logger): Try[Tree] = {
+
+    def _inner(_tree: Tree): Tree = {
+      val byte = reader.read()
+      if (byte == -1) {
+        _tree
+      } else {
+        val bit: Boolean = byte.toChar == '1'
+        if (bit) {
+          // leaf
+          val code: String = Stream.continually(reader.read()).takeWhile(_ != -1).map(_.toChar).take(8).mkString
+          val value: Byte = code.foldLeft(0)((a, l) => a * 2 + l - '0').toByte
+          val leaf = Leaf(value, -1)
+          logger.trace(f"L[x${value}%02X=${code}] = ${leaf}")
+          leaf
+        } else {
+          //node
+          logger.trace("N")
+          Node(_inner(_tree), _inner(_tree))
+        }
+      }
+    }
+
+    Try(_inner(null))
+  }
+
   def fromFile(file: File): CompressedData = {
     ???
   }
 }
 
-class CompressorFileIO(data: CompressedData) {
+class CompressorFileIO(data: CompressedData)(implicit logger: Logger) {
 
-  def toFile(file: File): Try[Boolean] = {
+  def writeAll(os: Writer): Try[Boolean] = {
     for {
-      os <- Try(new FileOutputStream(file))
-      h <- writeHeader(os)
+      //os <- Try(new FileWriter(file))
+      hSum <- writeHeader(os)
+      t <- writeTree(os)
       b <- writeBody(os)
       c <- writeChecksum(os)
       e <- Try(os.close())
@@ -24,17 +53,51 @@ class CompressorFileIO(data: CompressedData) {
     }
   }
 
-  protected def writeHeader(os: FileOutputStream): Try[Boolean] = {
+  protected def writeHeader(os: Writer): Try[Long] = {
+    for {
+      marker <- Try(os.write("BWSFZ"))
+    } yield {
+      0L
+    }
+  }
+
+   def writeTree(os: Writer): Try[Boolean] = {
+
     Try {
+      def _inner(tree: Tree): Unit = tree match {
+        case Node(left, right) =>
+          logger.trace("N")
+          os.write("0")
+          _inner(left)
+          _inner(right)
+        case Leaf(value,_) =>
+          val binStr = f"${value.toBinaryString}%8s".replaceAll(" ", "0")
+          logger.trace(f"L[x${value}%02x=$binStr]")
+          os.write(s"1${binStr}")
+      }
+      _inner(data.tree)
       true
     }
   }
 
-  protected def writeBody(os: FileOutputStream): Try[Boolean] = ???
 
-  protected def writeChecksum(os: FileOutputStream): Try[Boolean] = ???
+  protected def writeBody(os: Writer): Try[Boolean] = {
+    Try {
+      @tailrec
+      def _inner(bits: Bits): Unit = {
+        if (bits.nonEmpty) {
+          if(bits.head) os.write('1') else os.write('0')
+          _inner(bits.tail)
+        }
+      }
+      _inner(data.bits)
+      true
+    }
+  }
 
-  private def writeBits(os: FileOutputStream, bs: Bits): Try[Int] = {
+  protected def writeChecksum(os: Writer): Try[Boolean] = ???
+
+  private def writeBits(os: Writer, bs: Bits): Try[Int] = {
     ???
   }
 
